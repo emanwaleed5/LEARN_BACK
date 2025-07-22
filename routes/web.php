4cdf85d8b5d8661b1d5adcd5fb3ce4b4eb6c384c
emanwaleed5 <ewaleed188@gmail.com>
2025-07-22 11:13:58 +0300

    new webPage

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
 });
+
+Route::get('/contact-us', function () {
+    return 'contact-us';
+});
+
